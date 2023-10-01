9a770a879be4b07176d67029e04ef36d2e8796ab
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-01 16:04:30 -0700

    Write psuedocode

---
+++ main.js
@@ -0,0 +1,5 @@
+// Create a FUNCTION called getComputerChoice that will randomly return either 'Rock', 'Paper', of 'Scissors'.
+// Use console.log() to check if function works
+// Create a FUNCTION that plays a single round of Rock Paper Scissors that takes TWO PARAMETERS: playerSelection and computerSelection
+// Return the STRING that declares the winner of the rounds. For example, 'You Lose! Paper beats Rock'
+// Make your function's playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK, etc.)
