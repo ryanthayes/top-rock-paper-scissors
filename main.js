b0d9ceb037d3fe7b606d26f5f4017c1893e01017
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-02 08:21:40 -0700

    Write function to play round 5 times

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 // Create a FUNCTION called getComputerChoice that will randomly return either 'Rock', 'Paper', of 'Scissors'.
-// Use console.log() to check if function works
 
 function getComputerChoice() {
     const choice = ["rock", "paper", "scissors"];
@@ -8,29 +7,35 @@ function getComputerChoice() {
 
 // Create a FUNCTION that plays a single round of Rock Paper Scissors that takes TWO PARAMETERS: playerSelection and computerSelection
 // Return the STRING that declares the winner of the rounds. For example, 'You Lose! Paper beats Rock'
-// Make your function's playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK, etc.)
-
-let playerSelection = prompt("Ready Player One: Please choose either rock, paper or scissors, and type into the box below.").toLowerCase();
-let computerSelection = getComputerChoice();
-
-console.log(`Your choice is ${playerSelection}, the computer's choice is ${computerSelection}.`);
 
 function playRound(playerSelection, computerSelection){
-if (playerSelection === computerSelection){
-    return 'The result is a tie!';
-} else if (
-    (playerSelection === 'rock' && computerSelection === 'scissors') || 
-    (playerSelection === 'scissors' && computerSelection === 'paper') || 
-    (playerSelection === 'paper' && computerSelection === 'rock')
-    ){
-    return 'You win!';
-} else {
-    return 'You lose!';
-}
+    if (playerSelection === computerSelection){
+        return 'The result is a tie!';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') || 
+        (playerSelection === 'scissors' && computerSelection === 'paper') || 
+        (playerSelection === 'paper' && computerSelection === 'rock')
+        ){
+        return 'You win!';
+    } else {
+        return 'You lose!';
+    }
 } 
 
-console.log(playRound(playerSelection, computerSelection));
+// Write FUNCTION called game() that will play a round 5 times
 
-// Write FUNCTION called game()
-// Nest the previous function inside of game() to play a 5 round 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        
+        // Make your function's playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK, etc.)  
+        let playerSelection = prompt("Ready Player One: Please choose either rock, paper or scissors, and type into the box below.").toLowerCase(); 
+        
+        let computerSelection = getComputerChoice();
 
+        // Print results of player choice and computer choice to verify logic works
+        console.log(`Your choice is ${playerSelection}, the computer's choice is ${computerSelection}.`);
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
