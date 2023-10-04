f1df8f82b7024b2cb5a01337264abed88984cbcd
ryanthayes <127283656+ryanthayes@users.noreply.github.com>
2023-10-04 13:22:24 -0700

    Moved test code to test.js' git status git commit -m 'Moved test code to test.js'

--- test.js
+++ test.js
@@ -16,38 +16,65 @@ if (playerSelection === computerSelection){
 // Test switch 
 
 function playRound(playerSelection, computerSelection){
-    switch (playerSelection, computerSelection){
-        case 'rock, scissor':
-        case 'scissor, paper':
-        case 'paper, rock':
+    switch (playerSelection + computerSelection){
+        case 'rockscissor':
+        case 'scissorpaper':
+        case 'paperrock':
             return 'You win!'
             break
-        case 'rock, paper':
-        case 'paper, scissor':
-        case 'scissor, rock':  
+        case 'rockpaper':
+        case 'paperscissor':
+        case 'scissorrock':  
             return 'You lose!'
             break
-        case 'rock, rock':
-        case 'paper, paper':
-        case 'scissor, scissor':  
+        case 'rockrock':
+        case 'paperpaper':
+        case 'scissorscissor':  
             return 'You draw!'
             break
     }
 }
 
-// Create a FUNCTION that plays a single round of Rock Paper Scissors that takes TWO PARAMETERS: playerSelection and computerSelection
-// Return the STRING that declares the winner of the rounds. For example, 'You Lose! Paper beats Rock'
+// Event listeners for player buttons original
 
-function playRound(playerSelection, computerSelection){
-    if (playerSelection === computerSelection){
-        return 'The result is a tie!';
-    } else if (
-        (playerSelection === 'rock' && computerSelection === 'scissors') || 
-        (playerSelection === 'scissors' && computerSelection === 'paper') || 
-        (playerSelection === 'paper' && computerSelection === 'rock')
-        ){
-        return 'You win!';
-    } else {
-        return 'You lose!';
-    }
-} 
+const btnRockPlayer = document.querySelector('.btn-rock--player');
+const btnPaperPlayer = document.querySelector('.btn-paper--player');
+const btnScissorsPlayer = document.querySelector('.btn-scissors--player');
+
+btnRockPlayer.addEventListener('click', () => {
+    let playerSelection = 'Rock';
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+    isGameOver(playerScore, computerScore);
+})
+btnPaperPlayer.addEventListener('click', () => {
+    let playerSelection = 'Paper';
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+btnScissorsPlayer.addEventListener('click', () => {
+    let playerSelection = 'Scissors';
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+})
+
+console.log(`Your choice is ${playerSelection}, the computer's choice is ${computerSelection}.`);
+
+
+// Write FUNCTION called game() that will play a round 5 times
+
+// function game() {
+//     for (let i = 0; i < 5; i++) {
+        
+//         // Make your function's playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK, etc.)  
+//         let playerSelection = prompt("Ready Player One: Please choose either rock, paper or scissors, and type into the box below.").toLowerCase(); 
+        
+//         let computerSelection = getComputerChoice();
+
+//         // Print results of player choice and computer choice to verify logic works
+//         console.log(`Your choice is ${playerSelection}, the computer's choice is ${computerSelection}.`);
+//         console.log(playRound(playerSelection, computerSelection));
+//     }
+// }
+
+// game();
